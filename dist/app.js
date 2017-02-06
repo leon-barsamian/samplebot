7959895a8560ea238706f415f59bed3ca5e992ae
Leon BARSAMIAN <Leon.BARSAMIAN.contractor@euronext.com>
2017-02-06 15:17:49 +0100

    adding new app.js

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@
 const restify = require("restify");
 // const {restify} = require('restify');
 const builder = require("botbuilder");
+const demoConnector_1 = require("./connectors/demoConnector/demoConnector");
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -24,14 +25,57 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
-bot.dialog('/', [
+/*bot.dialog('/', [
     function (session) {
+        
         var arrayDebug = [session.message.user.id,
-            session.message.user.name,
-            session.message.address.channelId,
-            //session.message.address.id,
-            session.message.address.conversation.id
+        session.message.user.name,
+        session.message.address.channelId,
+        //session.message.address.id,
+        session.message.address.conversation.id
         ];
-        builder.Prompts.text(session, "conversation id : " + session.message.address.conversation.id);
+        builder.Prompts.text(session, "conversation id : "+session.message.address.conversation.id);
+        let card = createHeroCard(session);
+         var msg = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.list)
+            .attachments([card]);
+
+        session.send(msg);
     }
-]);
+]);*/
+let demoConnector = new demoConnector_1.DemoConnector();
+/*bot.dialog('/', new builder.IntentDialog()
+    .matches(demoConnector.getDialogId(), demoConnector.getDialog())
+    .onDefault(function (session) {
+        builder.Prompts.text(session, "conversation id : "+session.message.address.conversation.id);
+        let card = createHeroCard(session);
+         var msg = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.list)
+            .attachments([card]);
+
+        session.send(msg);
+    }));*/
+bot.dialog('/', new builder.IntentDialog()
+    .matches(demoConnector.getDialogPattern(), demoConnector.getDialog())
+    .onDefault(function (session) {
+    session.send("I didn't understand. Say hello to me!");
+}));
+function createHeroCard(session) {
+    return new builder.HeroCard(session)
+        .title('BotFramework Hero Card')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .buttons(getSampleCardActions(session));
+}
+function getSampleCardImages(session) {
+    return [
+        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+    ];
+}
+function getSampleCardActions(session) {
+    return [
+        builder.CardAction.imBack(session, "Plop Imback message", "imback title"),
+        builder.CardAction.postBack(session, "postBack message", "postBack title")
+    ];
+}
+//# sourceMappingURL=app.js.map
